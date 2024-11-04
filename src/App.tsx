de73b1f661a47eb00a3acfd38af9c3d40620ab2a
Landry Geiger <landry.geiger@gmail.com>
2024-11-04 00:52:03 -0600

    Fix application centering on xl screens

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="max-w-screen-xl px-10 md:px-20 lg:px-28 py-5 flex flex-col gap-12 lg:gap-20">
-      <Hero />
-      <Experience />
+    <div className="flex justify-center">
+      <div className="max-w-screen-xl px-10 md:px-20 lg:px-28 py-5 flex flex-col gap-12 lg:gap-20">
+        <Hero />
+        <Experience />
+      </div>
     </div>
   );
 }
