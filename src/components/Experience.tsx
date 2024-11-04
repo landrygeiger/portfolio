2c15f2f9f22c1874d337f03ca4bd13d29cb1a3e2
Landry Geiger <landry.geiger@gmail.com>
2024-11-04 00:02:37 -0600

    Add dates to experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,33 +7,39 @@ const experiences = [
     position: "Software Engineering Intern",
     imgSrc:
       "https://framerusercontent.com/images/YgKuMbfGEe8wEtKnYPdvnYefG2w.png?scale-down-to=1024",
+    timePeriod: "May. 2024 - Aug. 2024",
   },
   {
     company: "Hudl",
     position: "Software Engineering Intern",
     imgSrc:
       "https://www.sinclairhille.com/sites/default/files/styles/banner-copy/public/hudl_landscape_foreground_website.jpg?itok=QqqSXpdZ",
+    timePeriod: "May. 2023 - Aug. 2023",
   },
   {
     company: "Kiewit",
     position: "Platform Operations Intern",
     imgSrc: "https://www.kiewit.com/wp-content/uploads/2021/02/KHQ-10.jpg",
+    timePeriod: "May. 2022 - Aug. 2022",
   },
   {
     company: "Privy AI",
     position: "Software Engineer",
     imgSrc: "",
+    timePeriod: "Aug. 2024 - Current",
   },
   {
     company: "Cattle Kettle",
     position: "Software Engineer",
     imgSrc: "",
+    timePeriod: "Aug. 2023 - May. 2024",
   },
   {
     company: "UNL School of Computing",
     position: "SWE IV Teaching Assistant",
     imgSrc:
       "https://miro.medium.com/v2/resize:fit:1400/1*tkBROuzOQU6wJLZY7EN9LA.jpeg",
+    timePeriod: "Jan. 2024 - May. 2024",
   },
 ];
 
@@ -48,6 +54,7 @@ const Experience: FC = () => {
             company={experience.company}
             position={experience.position}
             imgSrc={experience.imgSrc}
+            timePeriod={experience.timePeriod}
           />
         ))}
       </div>
--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -5,9 +5,15 @@ type Props = {
   company: string;
   position?: string;
   imgSrc: string;
+  timePeriod?: string;
 };
 
-const ExperienceItem: FC<Props> = ({ company, position, imgSrc }) => {
+const ExperienceItem: FC<Props> = ({
+  company,
+  position,
+  imgSrc,
+  timePeriod,
+}) => {
   return (
     <div className="rounded-lg rounded-b-lg bg-white h-48 col-span-1 transition hover:scale-103 overflow-hidden relative flex flex-col justify-end">
       <div className="relative z-20 px-3 py-2 bg-gray-800 rounded-b-lg">
@@ -19,6 +25,7 @@ const ExperienceItem: FC<Props> = ({ company, position, imgSrc }) => {
             {position}
           </p>
         )}
+        <p className="text-gray-300 text-sm">{timePeriod}</p>
       </div>
       <img src={imgSrc} className="bg-img absolute z-10" />
     </div>
