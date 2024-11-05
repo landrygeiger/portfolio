baafef9c789a16e7902388bf3e1d31c3bfa5204a
Landry Geiger <landry.geiger@gmail.com>
2024-11-04 20:35:02 -0600

    Change experience list header size

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const ExperienceList: FC<Props> = ({ sectionTitle, experiences }) => (
   <div>
-    <h1 className="font-lemon text-white text-3xl lg:text-4xl">
+    <h1 className="font-lemon text-white text-4xl lg:text-5xl">
       {sectionTitle}
     </h1>
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
