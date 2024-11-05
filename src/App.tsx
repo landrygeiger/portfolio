62618f77ad7d2c99ca56cab36c62e4357ac1c8db
Landry Geiger <landry.geiger@gmail.com>
2024-11-04 20:20:07 -0600

    Adjust app padding values

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 function App() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-screen-xl px-10 md:px-20 lg:px-28 pt-5 pb-10 flex flex-col gap-12 lg:gap-20">
+      <div className="max-w-screen-xl px-8 md:px-12 pt-5 pb-10 flex flex-col gap-12 lg:gap-20">
         <Hero />
         <ExperienceList sectionTitle="Experience" experiences={JOBS} />
         <ExperienceList sectionTitle="Projects" experiences={PROJECTS} />
